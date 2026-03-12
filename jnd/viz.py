01bbb8c04642ade760dbca694590716f4ce4c44e
Andrey Barsky <andreybarsky@gmail.com>
2026-03-12 15:29:50 +0100

    updated visualisation funcs

--- jnd/viz.py
+++ jnd/viz.py
@@ -25,13 +25,19 @@ view_distance_inches = view_distance_cm * INCH_PER_CM
 
 
 ### pixels per degree:
-def get_ppd():
-    ppd = calc_dpi * view_distance_inches * np.tan(np.pi/180)
+def get_ppd(dpi=calc_dpi):
+    ppd = dpi * view_distance_inches * np.tan(np.pi/180)
     print(f'Pixels per degree: {ppd:.2f}')
     return ppd
     
 
-def rshow(img: np.ndarray, title=None, normalised=True, dpi=None, figsize=None, colorbar=False, show=True, **kwargs):
+def rshow(img: np.ndarray, 
+          title=None, 
+          normalised=True, 
+          dpi=None, 
+          figsize=None, 
+          colorbar=False, 
+          show=True, **kwargs):
     """Function that displays an image at exactly native resolution"""
     if dpi is None:
         if 'plot_dpi' in calib:
@@ -49,11 +55,22 @@ def rshow(img: np.ndarray, title=None, normalised=True, dpi=None, figsize=None, 
     if figsize is None:
         # show at native resolution
         h_px, w_px = img.shape[:2]
-        figsize = w_px / float(dpi), h_px / float(dpi)
-    plt.figure(figsize=figsize)
+        
+        pad_h = 80  # pixels for title + status text
+        # figsize = (WIDTH_PX / MONITOR_DPI, (HEIGHT_PX + pad_h) / MONITOR_DPI)
+        figsize = w_px / float(dpi), (h_px+pad_h) / float(dpi)
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+        ax = fig.add_axes([0, pad_h/(h_px+pad_h), 1, h_px/(h_px+pad_h)])        
+        
+        plt.yticks([])
+        # figsize = w_px / float(dpi), (h_px+pad_h) / float(dpi)
+        # plt.figure(figsize=figsize)
+    else:
+        fig = plt.figure(figsize=figsize)
+        ax = plt.gca()
     if normalised:
         img = normalise(img)
-    plt.imshow(img, **kwargs)
+    ax.imshow(img, interpolation='none', **kwargs)
     plt.gca().set_title(title)
     if colorbar:
         plt.colorbar()
@@ -63,6 +80,117 @@ def rshow(img: np.ndarray, title=None, normalised=True, dpi=None, figsize=None, 
     else:
         return plt.gca()
 
+
+def rshows(imgs: list, # of np.ndarrays 
+          titles: list=None, # of strings
+          suptitle: str=None,
+          normalised: bool=True, 
+          dpi: float=None, 
+          figsize: tuple=None, 
+          colorbar: bool=False,
+          pad_h: int=80,
+          pad_w: int=40,
+          show: bool=True, **kwargs):
+    """as rshow, but for multiple img inputs in a row
+    (for in column/s, just use this or rshow sequentially)"""
+    if dpi is None:
+        if 'plot_dpi' in calib:
+            dpi = calib['plot_dpi'] # true corrected value for whatever rescaling browser+jupyter+matplotlib does
+        else:
+            dpi = calib['calc_dpi'] # estimated value calculated from monitor resolution and physical dimension
+    
+    # ensure list of ndarrays, record their :
+    img_arrs = []
+    img_heights = []
+    img_widths = []
+    
+    for img in imgs:
+        
+        if isinstance(img, Image.Image):
+            img = np.asarray(img)
+            
+        if normalised:
+            img = normalise(img)
+            
+        if len(img.shape) == 2:
+            # add dummy RGB channel:
+            img = np.stack([img]*3, axis=2)   
+            
+        h, w = img.shape[:2]
+        img_heights.append(h)
+        img_widths.append(w)
+        img_arrs.append(img)
+            
+
+    
+    titles = [None]*len(imgs) if titles is None   else titles
+    if figsize is None:
+        # show at native resolution
+        
+        # pad_h = 80  # pixels for title + status text
+        # pad_w = 40 # padding between individual plots, and between plots and outer figure margin
+        
+        total_h = max(img_heights) + pad_h
+        total_w = sum(img_widths) + pad_w*(len(imgs)+1)
+        
+        figh, figw = total_w / float(dpi), total_h / float(dpi) # figure height and width in inches
+        
+        # fig, axes = plt.subplots(1, len(imgs), figsize=figsize, dpi=dpi)
+        fig = plt.figure(figsize=(figh, figw), dpi=dpi)
+        figh_, figw_ = fig.get_size_inches()
+        print(f'Creating figure of size: ({figh_:.1f},{figw_:.1f}) inches')
+        
+        
+        for i, (img, title) in enumerate(zip(imgs, titles)):
+            # add each image as a new axis:
+            
+            
+            imh, imw = img.shape[:2] # image height and width in pixels
+            
+            # t = 0
+            l = pad_w*(i+1) + sum(img_widths[:i])
+            # r = sum(img_widths[:i+1]) + pad_w*(i+1)
+            b = total_h - imh - pad_h # top aligned
+            # b = total_h - pad_h # bottom aligned
+            ax_rect = [l/total_w, b/total_h, imw/total_w, imh/total_h]
+            print(f'Adding axis with dimensions (l/b/w/h): {[np.round(a, 3) for a in ax_rect]}')
+            
+            assert max(ax_rect) <= 1
+            
+            ax = fig.add_axes(ax_rect)
+            
+#                     ax = fig.add_axes([l=0, 
+#                                        b=pad_h/(h_px+pad_h), 
+#                                        w=1, 
+#                                        h= h_px/(h_px+pad_h)])        
+
+            
+            ax.imshow(img, interpolation='none', **kwargs)
+            ax.set_title(title)
+            ax.set_xticks([])
+            ax.set_yticks([])
+        
+        
+    else:
+        fig, axes = plt.subplots(1, len(imgs), figsize=figsize)
+        
+        for img, ax, title in zip(imgs, axes, titles):
+            ax.imshow(img, interpolation='none', **kwargs)
+            ax.set_title(title)
+            ax.set_xticks([])
+            ax.set_yticks([])
+        
+    plt.suptitle(suptitle)
+    if colorbar:
+        plt.colorbar()
+        
+    if show:
+        plt.show()
+    else:
+        return plt.gca()    
+    
+    
+    
 def normalise(arr):
     """ normalise an array to range 0-1"""
     arr = arr - arr.min()
